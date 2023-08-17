e0319d7f4d782948d73dde0523b8e0b403ad00d7
Yosef Saleh <yosef426@hotmail.com>
2023-08-17 10:30:53 -0400

    clean up

--- app/routes.py
+++ app/routes.py
@@ -9,8 +9,17 @@ def store_readings():
     device_id = data.get("id")
     readings = data.get("readings")
 
-    if not device_id or not readings:
-        return jsonify({"error": "Invalid data provided"}), 400
+    if not device_id:
+        return jsonify({"error": "No id was provided"}), 400
+
+    if not readings:
+        return jsonify({"error": "No readings were provided"}), 400
+
+    # Basic type validation
+    if not isinstance(device_id, str):
+        return jsonify({"error": "Invalid data type for device id"}), 400
+    if not isinstance(readings, list):
+        return jsonify({"error": "Invalid data type for readings"}), 400
 
     if device_id not in in_memory_db:
         in_memory_db[device_id] = {}
@@ -19,15 +28,22 @@ def store_readings():
         timestamp = reading.get("timestamp")
         count = reading.get("count")
 
-        if not timestamp or count is None:
-            return jsonify({"error": "Malformed reading provided"}), 400
+        if not timestamp:
+            return jsonify({"error": "Timestamp is missing in the reading"}), 400
+        if count is None:
+            return jsonify({"error": "Count is missing in the reading"}), 400
+
+        # Check the types for the reading data
+        if not isinstance(timestamp, str):
+            return jsonify({"error": "Invalid data type for timestamp"}), 400
+        if not isinstance(count, (int, float)):
+            return jsonify({"error": "Invalid data type for count"}), 400
 
         # If the reading for this timestamp already exists, ignore it.
         if timestamp not in in_memory_db[device_id]:
             in_memory_db[device_id][timestamp] = count
 
     return jsonify({"message": "Readings stored successfully"}), 200
-
 
 @app.route('/fetch_readings/<device_id>', methods=['GET'])
 def fetch_readings(device_id):
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -29,10 +29,43 @@ class TestRoutes(unittest.TestCase):
         readings = in_memory_db.get("36d5658a-6908-479e-887e-a949ec199272", {})
         self.assertNotIn("2021-08-14T12:08:15+01:00", readings)
 
+    def test_malformed_json(self):
+        response = self.app.post('/store_readings', data="this is not json")
+        self.assertEqual(response.status_code, 415)  # Expect a bad request response
+
+    def test_missing_fields(self):
+        data = {"id": "36d5658a-6908-479e-887e-a949ec199272"}  # missing "readings" field
+        response = self.app.post('/store_readings', json=data)
+        self.assertEqual(response.status_code, 400)
+
+    def test_invalid_data_types(self):
+        data = {
+            "id": "36d5658a-6908-479e-887e-a949ec199272",
+            "readings": [{"timestamp": "2021-08-14T12:08:15+01:00", "count": "invalid_type"}]
+        }
+        response = self.app.post('/store_readings', json=data)
+        self.assertEqual(response.status_code, 400)
+
     def test_fetch_readings(self):
         device_id = "36d5658a-6908-479e-887e-a949ec199272"
         response = self.app.get(f'/fetch_readings/{device_id}')
         self.assertEqual(response.status_code, 200)
+
+    def test_fetch_nonexistent_device(self):
+        non_existent_device_id = "some-random-id"
+        response = self.app.get(f'/fetch_readings/{non_existent_device_id}')
+        data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['readings'], [])
+
+    def test_fetch_without_data(self):
+        device_id = "36d5658a-6908-479e-887e-a949ec199272"
+        # Clear database to ensure it doesn't contain data for the given device_id
+        in_memory_db.clear()
+        response = self.app.get(f'/fetch_readings/{device_id}')
+        data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['readings'], [])
 
     def tearDown(self):
         in_memory_db.clear()
